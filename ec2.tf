d507f4bbcddda43294148f59a4bb8f85402fa7b2
AgathaE <98202809+neleodi@users.noreply.github.com>
2023-06-22 00:11:32 -0600

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,17 @@
 resource "aws_instance" "web" {
   ami           = var.AMI_ID
   instance_type = var.INSTANCE_TYPE
- 
-  tags = {
+  monitoring = true
+  iam_instance_profile = "EC2-ROLE-FOR-JENKINS"
+  ebs_optimized = true
+  metadata_options {
+    http_endpoint = "enabled"
+    http_tokens   = "required"
+  }
+  root_block_device {
+    encrypted     = true
+  }
+tags = {
     Name = var.name
   }
 }
